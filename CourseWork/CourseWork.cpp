752ea6fc34f62b7eb57e427f624b69b65c1b3b46
Shaw Ying <destinydy1213@gmail.com>
2019-10-16 15:41:50 +0100

    file in&output

--- CourseWork/CourseWork.cpp
+++ CourseWork/CourseWork.cpp
@@ -3,33 +3,40 @@
 
 using namespace std;
 
-void push_to_file(int* matrix,int length, int width, string filename) {
+void push_to_file(int*** matrix,int count, int length, int width, string filename) {
 	ofstream myfile(filename, ios::app);
 	if (myfile.is_open()) {
-		for (int i = 0; i < length * width; i++) {
-			if (i % width == length - 1) {
-				myfile << *(matrix + i) << endl ;
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < length; j++) {
+				for (int k = 0; k < width; k++) {
+					myfile << matrix[i][j][k];
+					myfile << " ";
+				}
+				myfile << endl;
 			}
-			else { 
-				myfile << *(matrix + i) << " "; 
-			}
+			myfile << endl;
 		}
 	}
 	myfile.close();
 }
 
-int** pull_from_file(string filename, const int length, const int width) {
+int*** pull_from_file(string filename, const int length, const int width) {
 	ifstream myfile(filename);
-	int **temp = new int*[length];
-	for (int i = 0; i < length; i++) {
-		temp[i] = new int[width];
+	int count = 0;
+	myfile >> count;
+	int*** temp = new int** [count];
+	for (int i = 0; i < count; i++) {
+		temp[i] = new int*[length];
+		for (int j = 0; j < length; j++) {
+			temp[i][j] = new int[width];
+		}
 	}
-	cout << myfile.is_open();
 	if (myfile.is_open()) {
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j < width; j++) {
-				myfile >> temp[i][j];
-				cout << temp[i][j];
+		for(int i = 0; i< count;i++)
+		for (int j = 0; j < length; j++) {
+			for (int k = 0; k < width; k++) {
+				myfile >> temp[i][j][k];
+				cout << temp[i][j][k];
 			}
 		}	
 	}
@@ -41,18 +48,21 @@ int main() {
 	int a[2][2] = {0,1,2,3};
 	int b[3][3] = { 3,3,3,3,3,3,3,3,3 };
 	int c[4][4] = { 4, 4, 4, 4, 4, 4, 4,4 ,4 ,4, 4,4, 4,4,4,4 };
-	int** d;
+	int*** d;
 	string filename = "example.txt";
-	//push_to_file(*a, 2, 2, filename);
-	//push_to_file(*b, 3, 3, filename);
-	//push_to_file(*c, 4, 4, filename);
+	/*push_to_file(*a, 2, 2, filename);
+	push_to_file(*b, 3, 3, filename);
+	push_to_file(*c, 4, 4, filename);*/
 	d = pull_from_file(filename, 2, 2);
-	for (int i = 0; i < 2; i++) {
+	push_to_file(d, 1, 2, 2, filename);
+	for (int i = 0; i < 1; i++) {
 		for (int j = 0; j < 2; j++) {
-			cout << d[i][j];
+			for (int k = 0; k < 2; k++) {
+				cout << d[i][j][k] << " ";
+			}
 		}
 	}
-
+	delete[] d;
 	return 0;
 }
 
